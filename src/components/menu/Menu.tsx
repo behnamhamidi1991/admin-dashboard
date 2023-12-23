4b60bc4af3ad89ed148558c6a964410f918c10bc
behnamhamidi1991 <eslp91@hotmail.com>
2023-12-23 16:27:05 +0330

    Menu is done, grid is done, chart bar begins.

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -6,16 +6,14 @@ const Menu = () => {
   return (
     <div className="menu">
       {menu.map((item) => (
-        <div className="item">
+        <div className="item" key={item.id}>
           <span className="title">{item.title}</span>
-          <Link to="/" className="listItem">
-            <img src="/home.svg" alt="home" />
-            <span className="listItemTitle">Home</span>
-          </Link>
-          <Link to="/" className="listItem">
-            <img src="/profile.svg" alt="home" />
-            <span className="listItemTitle">Profile</span>
-          </Link>
+          {item.listItems.map((listItem) => (
+            <Link to="/" className="listItem" key={listItem.id}>
+              <img src={listItem.icon} alt="home" />
+              <span className="listItemTitle">Home</span>
+            </Link>
+          ))}
         </div>
       ))}
     </div>
